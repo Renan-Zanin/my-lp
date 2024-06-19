2fd1245d323fe7585a18b564bdda0967ea851217
Renan Zanin <renan.oliveira.ismart@gmail.com>
2024-06-19 19:32:46 -0300

    adicionado observador de scroll em profile

--- app/profile/_components/NumbersCard.tsx
+++ app/profile/_components/NumbersCard.tsx
@@ -26,7 +26,9 @@ export default function NumbersCard({ amount, text }: NumbersCardProps) {
     <Card className="bg-orange-50 border-none shadow-none rounded-tl-[100px] py-12 px-8 flex items-center justify-center">
       <CardContent className="flex flex-col items-center justify-center">
         <span className="text-8xl text-orange-900 font-bold mb-2">
-          <animated.span>{props.number.to((n) => n.toFixed(0))}</animated.span>
+          <animated.span ref={ref}>
+            {props.number.to((n) => n.toFixed(0))}
+          </animated.span>
         </span>
         <p className="text-black">{text}</p>
       </CardContent>
